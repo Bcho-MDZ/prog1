0a7825c91d8169714dfd39c8e1d3e1ddc9b672ce
roi <rodrinoya.1@gmail.com>
2018-09-01 18:57:48 -0300

    Agregado ejercicio 1 del tp1

---
+++ tp1/ejercicio1.php
@@ -0,0 +1,16 @@
+<html>
+    <head>
+        <title>Prueba variables</title>
+    </head>
+    <body>
+        <h2>Ejercicio Nro 1</h2>
+        <?php
+        $var=2018;
+        echo "Primer valor: $var\n";
+        $string="Rodrigo";
+        echo "Nombre: $otravar \n";
+        $otrastring='Noya';
+        echo "Apellido: $otrastring";
+        ?>
+    </body>
+</html>
