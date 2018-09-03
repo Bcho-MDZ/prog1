19d771bd1cb9123648172e6b6a49d17f60acab71
roi <rodrinoya.1@gmail.com>
2018-09-02 21:31:37 -0300

    Agregado el ejercicio 3

---
+++ tp1/ejercicio3.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Ejercicio 3</title>
+</head>
+<body>
+	<?php 
+		for ($i=1; $i <= 10; $i++) { 
+			echo "El valor de la variable es: $i<br>";
+		}
+
+		echo "<br>";
+		$var='a';
+		for ($i=1; $i <= 10; $i++) {
+			$var++;
+			echo "El valor de la variable es: $var<br>";
+		}
+
+		echo "<br>";
+		$var='r';
+		for ($i=1; $i <= 10; $i++) {
+			$var++;
+			echo "El valor de la variable es: $var<br>";
+		}
+
+		echo "<br>";
+		$var='a1';
+		for ($i=1; $i <= 10; $i++) {
+			$var++;
+			echo "El valor de la variable es: $var<br>";
+		}
+
+		echo "<br>";
+		$var='a01';
+		for ($i=1; $i <= 10; $i++) {
+			$var++;
+			echo "El valor de la variable es: $var<br>";
+		}
+
+		echo "<br>";
+		$var='a90';
+		for ($i=1; $i <= 10; $i++) {
+			$var++;
+			echo "El valor de la variable es: $var<br>";
+		}
+	 ?>
+</body>
+</html>
