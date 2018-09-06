920a53f660358b7ac792ab1b361cdf8f3696e66c
roi <rodrinoya.1@gmail.com>
2018-09-06 12:29:31 -0300

    Agregado el ejercicio 8

---
+++ tp1/ejercicio8.php
@@ -0,0 +1,14 @@
+<table>
+    <?php
+    $z=0;
+    for($x=0;$x<5;$x++){
+        echo "<tr>";
+        for($y=0;$y<5;$y++) {
+            echo "<th>$z</th>";
+            $z++;
+        }
+        echo "</tr>";
+    }
+    ?>
+</table>
+
