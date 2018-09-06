fa41f80882655d868b1f2029c6215f7d6c2f41cf
roi <rodrinoya.1@gmail.com>
2018-09-06 12:30:03 -0300

    cambie la funcion unique

--- tp1/ejercicio6.php
+++ tp1/ejercicio6.php
@@ -13,6 +13,12 @@
 		$arreglo[1]=0;
 		echo "Arreglo original: <br>";
 		print_r($arreglo);
+
+    /*echo "<br><br>arreglo sin duplicados:<br>";
+    $array = array_unique($array);
+    foreach ($array as $value){
+    echo "$value\n";*/
+
 		foreach ($arreglo as $key => $value) {
 			for ($i=0; $i < 15; $i++) { 
 				if ($arreglo2[$key]==$value) {
