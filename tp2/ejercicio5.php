f0ba930b388a027eae4ad5f62d7b3783a565105f
roi <rodrinoya.1@gmail.com>
2018-09-09 17:50:54 -0300

    modificado ejercicio 5

--- tp2/ejercicio5.php
+++ tp2/ejercicio5.php
@@ -1,7 +1,7 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: roi
- * Date: 09/09/18
- * Time: 17:39
- */
+<pre>
+<a href=receptorejercicio3.php?texto1=bla&texto2=ble&hidden=oculto1&pass=contrasenia1&check1=1&radio1=1&radio2=1&lista=op1>Pagina 1</a>
+<a href=receptorejercicio3.php?texto1=ble&texto2=bla&hidden=oculto2&pass=contrasenia2&check2=1&radio1=2&radio2=2&lista=op2>Pagina 2</a>
+<a href=receptorejercicio3.php?texto1=bla&texto2=ble&hidden=oculto3&pass=contrasenia3&check3=1&radio1=3&radio2=3&lista=op3>Pagina 3</a>
+<a href=receptorejercicio3.php?texto1=bla&texto2=ble&hidden=oculto4&pass=contrasenia4&check4=1&radio1=4&radio2=4&lista=op4>Pagina 4</a>
+
+</pre>
