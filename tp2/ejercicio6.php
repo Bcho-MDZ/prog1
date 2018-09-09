763c0de76332608c2a0b00922803148ead5bf053
roi <rodrinoya.1@gmail.com>
2018-09-09 18:09:08 -0300

    agregado ejercicio 6

---
+++ tp2/ejercicio6.php
@@ -0,0 +1,13 @@
+<pre>
+    <form action="receptor6.php" method="get" enctype="multipart/form-data">
+        <div>
+            <label for="filas">FILAS: </label>
+            <input type="text" name="filas" placeholder="ingrese la cantidad de filas...">
+            <br>
+            <label for="columnas">COLUMNAS: </label>
+            <input type="text" name="columnas" placeholder="ingrese la cantidad de columnas...">
+            <br>
+            <input type="submit" value="Crear tabla">
+        </div>
+    </form>
+</pre>
