6a78beac110898c0f006221fb1e4ac8027916a5f
roi <rodrinoya.1@gmail.com>
2018-09-09 17:50:06 -0300

    modificacion ejercicio 4

--- tp2/ejercicio4.php
+++ tp2/ejercicio4.php
@@ -1,7 +1,5 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: roi
- * Date: 09/09/18
- * Time: 17:19
- */
+<pre>
+<a href=receptorget.php>Pagina vacia<br></a>
+<a href=receptorget.php?dato=1>Pagina 1<br></a>
+<a href=receptorget.php?dato1=1&dato2=segundo&dato3=tercero>Pagina 2</a>
+</pre>
