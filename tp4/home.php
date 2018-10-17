42635fa22c4e4815f4c863448322a769d8f85283
roi <rodrinoya.1@gmail.com>
2018-10-17 13:08:10 -0300

    unos comentarios

--- tp4/home.php
+++ tp4/home.php
@@ -1,7 +1,9 @@
 <pre>
 <?php
+  //trata de iniciar la sesion, si sale todo bien se fija si hay alguna sesion iniciada, si no hay te manda a loguear, si hay te saluda
   if (session_start()) {
     if ($_SESSION[habilitado]!=1) {
+      //a la casa a loguearse
       header("location: http://localhost/programacion1/tp4/ejercicio7.php");
     }
     echo "<h3>..-Bienvenido/a de nuevo {$_SESSION[usuario]}-..";
